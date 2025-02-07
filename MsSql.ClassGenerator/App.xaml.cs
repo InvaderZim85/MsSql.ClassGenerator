166380a5fb8337072f37ece31aa00951afa11bc1
Andreas <apouwels85@gmail.com>
2025-02-07 19:41:38 +0100

    Add project files.

---
+++ MsSql.ClassGenerator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MsSql.ClassGenerator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
