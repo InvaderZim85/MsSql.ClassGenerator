750e8b3b2a876db0acb91319582fd90477e52ac4
Andreas <apouwels85@gmail.com>
2025-02-18 19:36:46 +0100

    Added code window

---
+++ MsSql.ClassGenerator/Ui/View/CodeWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MsSql.ClassGenerator.Ui.View;
+
+/// <summary>
+/// Interaction logic for CodeWindow.xaml
+/// </summary>
+public partial class CodeWindow : MetroWindow
+{
+    /// <summary>
+    /// Contains the code which should be shown.
+    /// </summary>
+    private readonly string _code;
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="CodeWindow"/>.
+    /// </summary>
+    /// <param name="code">The code which should be shown.</param>
+    public CodeWindow(string code)
+    {
+        InitializeComponent();
+
+        _code = code;
+    }
+}
